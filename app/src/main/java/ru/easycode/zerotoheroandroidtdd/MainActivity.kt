c993548a429b1c1b4c242a4f1864a448311db397
George_Magpie <george.magpie.94@gmail.com>
2023-12-04 22:34:14 +0300

    task/ 007 solved. Пиршлось немножко потупить))

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,29 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var string : String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.changeButton.setOnClickListener {
+            binding.titleTextView.text = "I am an Android Developer!"
+        }
+        if (savedInstanceState != null){
+            string = savedInstanceState.getString("string").toString()
+            binding.titleTextView.text = string
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("string", binding.titleTextView.text.toString() )
     }
 }
