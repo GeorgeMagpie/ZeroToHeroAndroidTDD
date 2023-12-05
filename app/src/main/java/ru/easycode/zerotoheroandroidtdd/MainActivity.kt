f4e1c66a1b3efd480a43ad8ca122eaf521a5afa7
George_Magpie <george.magpie.94@gmail.com>
2023-12-05 07:29:40 +0300

    task/007 solved update

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,11 +2,12 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var string : String
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,14 +18,15 @@ class MainActivity : AppCompatActivity() {
         binding.changeButton.setOnClickListener {
             binding.titleTextView.text = "I am an Android Developer!"
         }
-        if (savedInstanceState != null){
-            string = savedInstanceState.getString("string").toString()
-            binding.titleTextView.text = string
-        }
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
         outState.putString("string", binding.titleTextView.text.toString() )
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        binding.titleTextView.text = savedInstanceState.getString("string")
     }
 }
